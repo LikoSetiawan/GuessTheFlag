e959536d8b40dffc8584b71304042a411a7ea13d
LikoSetiawan <128354782+LikoSetiawan@users.noreply.github.com>
2024-01-31 17:05:25 +0800

    Initial commit

---
+++ GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Liko Setiawan on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
